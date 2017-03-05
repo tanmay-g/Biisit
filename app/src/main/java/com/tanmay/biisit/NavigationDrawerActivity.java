7be6b810701ef70c2b304e550226a904dc9ea595
tanmay-g <tanmay500@gmail.com>
2017-03-05 21:32:42 +0530

    Adding sign-in and sign-out
    Firebase auth is used for this, and the firebase auth UI library is used to handle the account related userflow

--- app/src/main/java/com/tanmay/biisit/NavigationDrawerActivity.java
+++ app/src/main/java/com/tanmay/biisit/NavigationDrawerActivity.java
@@ -1,10 +1,13 @@
 package com.tanmay.biisit;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
@@ -12,12 +15,25 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.tanmay.biisit.myMusic.MyMusicFragment;
 
 public class NavigationDrawerActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int RC_SIGN_IN = 111;
+    NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +48,34 @@ public class NavigationDrawerActivity extends AppCompatActivity
 //        drawer.setDrawerListener(toggle);
 //        toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
         if (savedInstanceState == null) {
 //            getSupportFragmentManager().beginTransaction().replace(R.id.content_navigation_drawer, new MyMusicFragment()).commit();
-            onNavigationItemSelected(navigationView.getMenu().getItem(0));
+            onNavigationItemSelected(mNavigationView.getMenu().getItem(0));
+        }
+        updateUserDisplay();
+    }
+
+    private void updateUserDisplay(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+
+            if (mNavigationView == null)
+                return;
+            View header = mNavigationView.getHeaderView(0);
+            ((TextView)header.findViewById(R.id.user_email)).setText(email);
+            ((TextView)header.findViewById(R.id.user_name)).setText(name);
+        }
+        else {
+            if (mNavigationView == null)
+                return;
+            View header = mNavigationView.getHeaderView(0);
+            ((TextView)header.findViewById(R.id.user_email)).setText("");
+            ((TextView)header.findViewById(R.id.user_name)).setText("Biisit User");
         }
     }
 
@@ -52,6 +91,8 @@ public class NavigationDrawerActivity extends AppCompatActivity
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        boolean accountStuff = false;
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -88,15 +129,96 @@ public class NavigationDrawerActivity extends AppCompatActivity
             getSupportFragmentManager().beginTransaction().replace(R.id.content_navigation_drawer, new Fragment()).commit();
 
         } else if (id == R.id.sign_in_out) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.content_navigation_drawer, new Fragment()).commit();
-
+            accountStuff = true;
+//            getSupportFragmentManager().beginTransaction().replace(R.id.content_navigation_drawer, new Fragment()).commit();
+            if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                AuthUI.getInstance()
+                        .signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                MenuItem signInOutMenuItem = mNavigationView.getMenu().findItem(R.id.sign_in_out);
+                                signInOutMenuItem.setTitle("Sign In");
+                                updateUserDisplay();
+                            }
+                        });
+            }
+            else {
+                startActivityForResult(
+                        AuthUI.getInstance().createSignInIntentBuilder()
+                            .setTheme(R.style.FirebaseOrangeTheme)
+//                                .setLogo(R.mipmap.ic_launcher)
+//                            .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build()))
+//                            .setTosUrl(getSelectedTosUrl())
+                                .setIsSmartLockEnabled(true)
+                                .setAllowNewEmailAccounts(true)
+//                            .setAllowNewEmailAccounts(mAllowNewEmailAccounts.isChecked())
+                                .build(),
+                        RC_SIGN_IN);
+            }
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        item.setChecked(true);
-        setTitle(item.getTitle());
+        if (! accountStuff) {
+            item.setChecked(true);
+            setTitle(item.getTitle());
+        }
+        else {
+            return false;
+//            item.setChecked(false);
+//            mNavigationView.getMenu().getItem(0).setChecked(true);
+//            mNavigationView.setCheckedItem(mNavigationView.getMenu().getItem(0).getItemId());
+        }
 
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+            return;
+        }
+
+        showSnackbar("Unexpected response from Sign-in");
+    }
+
+    @MainThread
+    private void handleSignInResponse(int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+
+        // Successfully signed in
+        if (resultCode == ResultCodes.OK) {
+            MenuItem signInOutMenuItem = mNavigationView.getMenu().findItem(R.id.sign_in_out);
+            signInOutMenuItem.setTitle("Sign Out");
+            updateUserDisplay();
+            return;
+        } else {
+            // Sign in failed
+            if (response == null) {
+                // User pressed back button
+                showSnackbar("Sign in was cancelled");
+                return;
+            }
+
+            if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                showSnackbar("Unable to sign in. Please check internet connection.");
+                return;
+            }
+
+            if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                showSnackbar("Error while signing in");
+                return;
+            }
+        }
+
+        showSnackbar("Unexpected response from Sign-in");
+    }
+
+    @MainThread
+    private void showSnackbar(String errorMessageRes) {
+        Snackbar.make(findViewById(R.id.drawer_layout), errorMessageRes, Snackbar.LENGTH_LONG).show();
     }
 }
